06814609e1df1ed63b1a703398c5bb3b2bef3bd0
Kathlene <kathlenepanugam@gmail.com>
2022-07-17 20:01:52 +0800

    Resume first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Personal_Resume_in_PDF_Format
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.GenPDFBtn = new System.Windows.Forms.Button();
+            this.OpenFileBtn = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.GenPDFBtn);
+            this.panel1.Controls.Add(this.OpenFileBtn);
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Location = new System.Drawing.Point(84, 45);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(424, 406);
+            this.panel1.TabIndex = 0;
+            // 
+            // GenPDFBtn
+            // 
+            this.GenPDFBtn.Location = new System.Drawing.Point(231, 357);
+            this.GenPDFBtn.Name = "GenPDFBtn";
+            this.GenPDFBtn.Size = new System.Drawing.Size(148, 23);
+            this.GenPDFBtn.TabIndex = 2;
+            this.GenPDFBtn.Text = "Generate to pdf";
+            this.GenPDFBtn.UseVisualStyleBackColor = true;
+            this.GenPDFBtn.Click += new System.EventHandler(this.GenPDFBtn_Click);
+            // 
+            // OpenFileBtn
+            // 
+            this.OpenFileBtn.Location = new System.Drawing.Point(135, 357);
+            this.OpenFileBtn.Name = "OpenFileBtn";
+            this.OpenFileBtn.Size = new System.Drawing.Size(90, 23);
+            this.OpenFileBtn.TabIndex = 1;
+            this.OpenFileBtn.Text = "Open File";
+            this.OpenFileBtn.UseVisualStyleBackColor = true;
+            this.OpenFileBtn.Click += new System.EventHandler(this.OpenFileBtn_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.richTextBox1.Location = new System.Drawing.Point(36, 121);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(343, 202);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(85, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 34);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "PDF Generator";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(260, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Using JSON file";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(36, 330);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(343, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(686, 508);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Resume";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button GenPDFBtn;
+        private System.Windows.Forms.Button OpenFileBtn;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using iTextSharp.text;
+using Newtonsoft.Json;
+using iTextSharp.text.pdf;
+
+namespace Personal_Resume_in_PDF_Format
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFileBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filename = openFileDialog.FileName;
+                textBox1.Text = filename;
+                richTextBox1.Text = File.ReadAllText(filename);
+            }
+        }
+
+        private void GenPDFBtn_Click(object sender, EventArgs e)
+        {
+            Document document = new Document();
+            PdfWriter.GetInstance(document, new FileStream("PANUGA_KATHLENE ANN.pdf", FileMode.Create));
+            document.Open();
+
+            Paragraph paragraph = new Paragraph(richTextBox1.Text);
+            document.Add(paragraph);
+            document.Close();
+            MessageBox.Show("PDF saved!");
+        }
+    }
+}
